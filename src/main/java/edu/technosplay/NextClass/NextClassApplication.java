fb300d6dc443739a0072c0ae41090c6087b7992f
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-05-02 20:32:06 -0300

    feat: add page serialization

--- src/main/java/edu/technosplay/NextClass/NextClassApplication.java
+++ src/main/java/edu/technosplay/NextClass/NextClassApplication.java
@@ -2,8 +2,10 @@ package edu.technosplay.NextClass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class NextClassApplication {
 
 	public static void main(String[] args) {
