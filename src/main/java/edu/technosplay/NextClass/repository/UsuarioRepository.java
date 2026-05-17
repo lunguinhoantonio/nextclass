f51e1905209bc38df6e2287a6390958675c4c672
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-05-17 14:36:34 -0300

    feat: add UsuarioRepository for connect to database

--- src/main/java/edu/technosplay/NextClass/repository/UsuarioRepository.java
+++ src/main/java/edu/technosplay/NextClass/repository/UsuarioRepository.java
@@ -1,27 +1,20 @@
 package edu.technosplay.NextClass.repository;
 
-import edu.technosplay.NextClass.model.Aluno;
-import edu.technosplay.NextClass.model.enums.StatusAluno;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import edu.technosplay.NextClass.model.Usuario;
+import edu.technosplay.NextClass.model.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Optional<Aluno> findByCpf(String cpf);
-    Optional<Aluno> findByEmail(String email);
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCpf(String cpf);
+    Optional<Usuario> findByEmail(String email);
     boolean existsByCpf(String cpf);
     boolean existsByEmail(String email);
-    Page<Aluno> findByStatus(StatusAluno status, Pageable pageable);
-
-    /*@Query("SELECT a FROM Aluno a WHERE " +
-            "LOWER(a.nome) LIKE LOWER(CONCAT('%', :termo, '%')) OR " +
-            "a.cpf LIKE CONCAT('%', :termo, '%') OR " +
-            "LOWER(a.email) LIKE LOWER(CONCAT('%', :termo, '%'))")
-    Page<Aluno> buscarPorTermo(@Param("termo") String termo, Pageable pageable);*/
+    List<Usuario> findAllByRole(Role role);
+    List<Usuario> findAllByAtivo(boolean ativo);
+    //List<Usuario> findByRole(Role role, boolean ativo);
 }
