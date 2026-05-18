69c4d971988ea808204af8e63141f63cfd8ff0eb
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-05-18 18:23:03 -0300

    feat: add repository to Curso

---
+++ src/main/java/edu/technosplay/NextClass/repository/CursoRepository.java
@@ -0,0 +1,7 @@
+package edu.technosplay.NextClass.repository;
+
+import edu.technosplay.NextClass.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
