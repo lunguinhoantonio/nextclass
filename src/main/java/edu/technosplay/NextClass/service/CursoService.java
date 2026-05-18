e43bd849787d4549a17234980bc5622573cba7cb
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-05-18 18:23:25 -0300

    feat: add service to Curso

---
+++ src/main/java/edu/technosplay/NextClass/service/CursoService.java
@@ -0,0 +1,9 @@
+package edu.technosplay.NextClass.service;
+
+import edu.technosplay.NextClass.dto.response.CursoResponse;
+
+import java.util.List;
+
+public interface CursoService {
+    List<CursoResponse> listar();
+}
