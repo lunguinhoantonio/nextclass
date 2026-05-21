851c04f3f686ce373dbe0eaee7d7d60fd5bc1433
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-05-21 20:49:21 -0300

    feat: add functions

--- src/main/java/edu/technosplay/NextClass/repository/CursoRepository.java
+++ src/main/java/edu/technosplay/NextClass/repository/CursoRepository.java
@@ -3,5 +3,9 @@ package edu.technosplay.NextClass.repository;
 import edu.technosplay.NextClass.model.Curso;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CursoRepository extends JpaRepository<Curso, Long> {
+    List<Curso> findAllByAtivo(boolean ativo);
+    List<Curso> findAllByProfessorId(Long professorId);
 }
