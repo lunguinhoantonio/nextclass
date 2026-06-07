448dd6e0527a9f34d1686394a8af58900066031f
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-06-06 22:48:00 -0300

    feat: add login method

--- src/main/java/edu/technosplay/NextClass/service/AuthService.java
+++ src/main/java/edu/technosplay/NextClass/service/AuthService.java
@@ -1,8 +1,11 @@
 package edu.technosplay.NextClass.service;
 
+import edu.technosplay.NextClass.dto.request.LoginRequest;
 import edu.technosplay.NextClass.dto.request.UsuarioRequest;
+import edu.technosplay.NextClass.dto.response.LoginResponse;
 import edu.technosplay.NextClass.dto.response.UsuarioResponse;
 
 public interface AuthService {
     UsuarioResponse registrar(UsuarioRequest request);
+    LoginResponse login(LoginRequest request);
 }
