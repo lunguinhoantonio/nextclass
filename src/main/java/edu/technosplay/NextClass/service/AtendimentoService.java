497104408b701322fb3d49a0d0da8e712b537daf
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-06-12 20:37:53 -0300

    feat: add new methods: abrirAutenticado and listar

--- src/main/java/edu/technosplay/NextClass/service/AtendimentoService.java
+++ src/main/java/edu/technosplay/NextClass/service/AtendimentoService.java
@@ -1,22 +1,24 @@
 package edu.technosplay.NextClass.service;
 
+import edu.technosplay.NextClass.dto.request.AtendimentoAutenticadoRequest;
 import edu.technosplay.NextClass.dto.request.AtendimentoRequest;
 import edu.technosplay.NextClass.dto.response.AtendimentoResponse;
+import edu.technosplay.NextClass.model.Usuario;
 import edu.technosplay.NextClass.model.enums.StatusAtendimento;
 import edu.technosplay.NextClass.model.enums.TipoAtendimento;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
 public interface AtendimentoService {
     AtendimentoResponse abrirPublico(AtendimentoRequest request);
+    AtendimentoResponse abrirAutenticado(AtendimentoAutenticadoRequest request, Usuario usuario);
     AtendimentoResponse abrir(Long solicitanteId, AtendimentoRequest request);
     AtendimentoResponse buscarPorId(Long id);
+    List<AtendimentoResponse> listar(TipoAtendimento tipo, StatusAtendimento status, Sort sort);
     List<AtendimentoResponse> listarPorSolicitante(Long solicitanteId);
     List<AtendimentoResponse> listarPorAtendente(Long atendenteId);
     List<AtendimentoResponse> listarSemAtendente();
-    List<AtendimentoResponse> listarPorStatus(StatusAtendimento status);
-    List<AtendimentoResponse> listarPorTipo(TipoAtendimento tipo);
-    List<AtendimentoResponse> listarPorTipoEStatus(TipoAtendimento tipo, StatusAtendimento status);
     AtendimentoResponse atribuirAtendente(Long atendimentoId, Long atendenteId);
     AtendimentoResponse atualizarStatus(Long id, StatusAtendimento novoStatus);
 }
