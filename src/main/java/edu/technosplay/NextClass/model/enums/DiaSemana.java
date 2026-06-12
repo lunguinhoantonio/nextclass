565aa4b6aff4bb3bcc637adae52d371ce1a1b343
lunguinhoantonio <antoniolunguinho007@gmail.com>
2026-06-12 20:35:17 -0300

    feat: add description to all days of week

--- src/main/java/edu/technosplay/NextClass/model/enums/DiaSemana.java
+++ src/main/java/edu/technosplay/NextClass/model/enums/DiaSemana.java
@@ -1,20 +1,26 @@
 package edu.technosplay.NextClass.model.enums;
 
+import lombok.Getter;
+
 import java.time.DayOfWeek;
 
 public enum DiaSemana {
-    SEGUNDA(DayOfWeek.MONDAY),
-    TERCA(DayOfWeek.TUESDAY),
-    QUARTA(DayOfWeek.WEDNESDAY),
-    QUINTA(DayOfWeek.THURSDAY),
-    SEXTA(DayOfWeek.FRIDAY),
-    SABADO(DayOfWeek.SATURDAY),
-    DOMINGO(DayOfWeek.SUNDAY);
+    SEGUNDA(DayOfWeek.MONDAY, "Segunda-feira"),
+    TERCA(DayOfWeek.TUESDAY, "Terça-feira"),
+    QUARTA(DayOfWeek.WEDNESDAY, "Quarta-feira"),
+    QUINTA(DayOfWeek.THURSDAY, "Quinta-feira"),
+    SEXTA(DayOfWeek.FRIDAY, "Sexta-feira"),
+    SABADO(DayOfWeek.SATURDAY, "Sábado"),
+    DOMINGO(DayOfWeek.SUNDAY, "Domingo");
 
     private final DayOfWeek dayOfWeek;
 
-    DiaSemana(DayOfWeek dayOfWeek) {
+    @Getter
+    private final String descricao;
+
+    DiaSemana(DayOfWeek dayOfWeek, String descricao) {
         this.dayOfWeek = dayOfWeek;
+        this.descricao = descricao;
     }
 
     public DayOfWeek toDayOfWeek() {
